e74a812be52fc2d26b1faf94a62cd0067c508ee6
ericksonmanurung <erickson.manurung23@gmail.com>
2021-06-09 23:44:44 +0700

    alur tampilan games

--- game-client/src/views/Home.vue
+++ game-client/src/views/Home.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>JUDUL: Peraturan</h1>
+    <ul>
+      <li>1. Buat room terlebih dahulu</li>
+      <li>2. Minimal permainan dimainakn 2 orang maksimal 5</li>
+      <li>3. Game akan dimulai ketika ketika pemain sudah ada 2 dan di klic start</li>
+      <li>4. akan ada beberapa kata random siapa yang lebih cepet menyelesaikan pemainan dia pemenanngya</li>
+    </ul>
+
+    <button class="btn btn-primary mb-4">Buat room</button>
+    <form>
+      <div class="row g-3 align-items-center">
+        <div class="col-auto">
+          <label for="inputPassword6" class="col-form-label">Nama Room</label>
+        </div>
+        <div class="col-auto">
+          <input type="text" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline">
+        </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
+
+<style>
+
+</style>
---
+++ game-client/src/views/Register.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <h1>JUDUL: menghitung cepat</h1>
+    <form>
+      <div class="mb-3">
+        <input type="text" class="form-control" placeholder="USERNAME">
+      </div>
+      <!-- <button type="submit" class="btn btn-primary">Enter</button> -->
+      <router-link to="/home" class="btn btn-primary">Enter</router-link>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
