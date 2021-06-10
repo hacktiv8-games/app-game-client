ec4c21041b2a3379be0d810318daab9f4a520fba
adsap <addesaputra11@gmail.com>
2021-06-10 16:31:50 +0700

    user connect, sent and listen user

--- game-client/src/App.vue
+++ game-client/src/App.vue
@@ -1,8 +1,40 @@
 <template>
   <div id="app">
     <router-view/>
+    <input type="text" v-model="username">
+    <button type="button" class="btn btn-primary text-center" @click.prevent="addUser">Submit</button>
+    <ul>
+      <li v-for="user, index in users" :key="index">
+        {{ user }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      username: ''
+    }
+  },
+  computed: {
+    users () {
+      return this.$store.state.users
+    }
+  },
+  methods: {
+    addUser () {
+      this.$store.dispatch('addUser', this.username)
+    }
+  },
+  mounted () {
+    this.$store.dispatch('userConnect')
+    this.$store.dispatch('userListen')
+  }
+}
+</script>
 
 <style>
 
--- game-client/src/store/index.js
+++ game-client/src/store/index.js
@@ -1,21 +1,52 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { io } from 'socket.io-client'
+
+const socket = io('http://localhost:3000')
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    id: '',
+    username: '',
+    users: []
   },
   mutations: {
+    setId (state, payload) {
+      state.id = payload
+    },
+    setUsername (state, payload) {
+      state.username = payload
+    },
+    setUsers (state, payload) {
+      state.users.push(payload)
+    }
   },
   actions: {
-    generateWord () {
-      let result           = ''
-      let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-      for (let i = 0; i < 5; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length))
-      }
-      return result
+    // generateWord () {
+    //   let result = ''
+    //   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    //   for (let i = 0; i < 5; i++) {
+    //     result += characters.charAt(Math.floor(Math.random() * characters.length))
+    //   }
+    //   return result
+    // },
+    addUser ({ commit }, payload) {
+      commit('setUsername', payload)
+      socket.emit('entry', { username: this.state.username })
+    },
+    userConnect ({ commit }) {
+      socket.on('connection', () => {
+        console.log(`connect with id : ${socket.id}`)
+        commit('setId', socket.id)
+      })
+    },
+    userListen ({ commit }) {
+      socket.on('clientUser', (payload) => {
+        const { username } = payload
+        commit('setUsers', username)
+      })
     }
   },
   modules: {
